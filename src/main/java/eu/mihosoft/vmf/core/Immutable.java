843aea6e11e115a9b7bdf2fb770f9058920bd625
Michael Hoffer <info@michaelhoffer.de>
2017-03-10 22:52:07 +0100

    added immutable annotation. immutable types are important and need to be implemented soon.

---
+++ src/main/java/eu/mihosoft/vmf/core/Immutable.java
@@ -0,0 +1,14 @@
+package eu.mihosoft.vmf.core;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by miho on 10.03.17.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Immutable {
+}
