3a9bfd7744e024380d2a48dbb987304d82c9a84a
Michael Hoffer <info@michaelhoffer.de>
2019-03-09 00:36:26 +0300

    getter only test extended

--- test-suite/src/test/java/eu/mihosoft/vmftest/getteronly/GetterOnlyTest.java
+++ test-suite/src/test/java/eu/mihosoft/vmftest/getteronly/GetterOnlyTest.java
@@ -15,5 +15,22 @@ public class GetterOnlyTest {
 
         Assert.assertTrue("immutable obj".equals(withName1.getName()));
         Assert.assertTrue("mutable obj".equals(withName2.getName()));
+
+        // setting immutable property: expected to fail
+        try {
+            immutableObj.vmf().reflect().propertyByName("name").ifPresent(p -> p.set("new name"));
+            Assert.fail("setting immutable property should not work");
+        } catch ( Exception ex ) {
+            ex.printStackTrace(System.err);
+        }
+
+
+        // setting mutable property: expected to work
+        try {
+            mutableObj.vmf().reflect().propertyByName("name").ifPresent(p -> p.set("new name"));
+        } catch ( Exception ex ) {
+            ex.printStackTrace(System.err);
+            Assert.fail("setting mutable property must work");
+        }
     }
 }
