ef3b651e73bd3b2bb21b49e201639184daddf7db
Michael Hoffer <info@michaelhoffer.de>
2019-04-05 22:10:43 +0200

    refers documentation part 1

--- core/src/main/java/eu/mihosoft/vmf/core/Refers.java
+++ core/src/main/java/eu/mihosoft/vmf/core/Refers.java
@@ -2,11 +2,29 @@ package eu.mihosoft.vmf.core;
 
 import java.lang.annotation.*;
 
+/**
+ * Used to define a cross reference (linking releationship). 
+ * <h3>Example Model:</h3>
+ * <pre><code>
+ * package mypkg.vmfmodel;
+ * 
+ * import eu.mihosoft.vmf.core.*;
+ *
+ * Todo add code
+ * 
+ * </code></pre>
+ * 
+ * <p>Created by miho on 12.03.2019.</p>
+ * 
+ * @see Contains
+ * @see <a href="https://github.com/miho/VMF-Tutorials/blob/master/VMF-Tutorial-03b/README.md">Tutorial on Cross References</a>
+ * @author Michael Hoffer <info@michaelhoffer.de>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Refers {
    /**
-     * Sets the opposite property of this cross-reference relationship, e.g., <b>"prop"</b> (short form) or <b>"ClassName.prop"</b> (full property).
+     * Sets the opposite property of this cross reference relationship, e.g., <b>"prop"</b> (short form) or <b>"ClassName.prop"</b> (full property).
      */
     String opposite();
 }
