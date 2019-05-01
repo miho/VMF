91561fd45329f3984b783a229851b0b9134a3350
Michael Hoffer <info@michaelhoffer.de>
2019-05-01 17:19:33 +0200

    grammarlang model from vmf-text works again

--- test-suite/src/test/vmf/eu/mihosoft/vmftest/complex/vmf_text/grammarmodel/vmfmodel/GrammarLangModel.java
+++ test-suite/src/test/vmf/eu/mihosoft/vmftest/complex/vmf_text/grammarmodel/vmfmodel/GrammarLangModel.java
@@ -23,245 +23,245 @@
 //  */
 package eu.mihosoft.vmftest.complex.vmf_text.grammarmodel.vmfmodel;
 
-interface GrammarModel{}
+// interface GrammarModel{}
 
-// import eu.mihosoft.vmf.core.*;
+import eu.mihosoft.vmf.core.*;
 
-// import java.util.List;
-// import java.util.Optional;
+import java.util.List;
+import java.util.Optional;
 
 
-// //@DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.CheckRulesDelegate")
-// interface GrammarModel {
-//     @Contains(opposite = "model")
-//     RuleClass[] getRuleClasses();
+//@DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.CheckRulesDelegate")
+interface GrammarModel {
+    @Contains(opposite = "model")
+    RuleClass[] getRuleClasses();
 
-//     String getGrammarName();
+    String getGrammarName();
 
-//     String getPackageName();
+    String getPackageName();
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.GetRootClassDelegate")
-// //    boolean hasRootClass();
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.GetRootClassDelegate")
+//    boolean hasRootClass();
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.GetRootClassDelegate")
-//     // RuleClass rootClass();
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.GetRootClassDelegate")
+    // RuleClass rootClass();
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.RuleClassLookup")
-//     // Optional<RuleClass> ruleClassByName(String name);
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.RuleClassLookup")
+    // Optional<RuleClass> ruleClassByName(String name);
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.RuleClassLookup")
-//     // Optional<Property> propertyByName(String ruleClassName, String propName);
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.RuleClassLookup")
+    // Optional<Property> propertyByName(String ruleClassName, String propName);
 
-//     @Contains(opposite = "model")
-//     TypeMappings getTypeMappings();
+    @Contains(opposite = "model")
+    TypeMappings getTypeMappings();
 
-//     @Contains(opposite = "model")
-//     CustomRule[] getCustomRules();
-// }
+    @Contains(opposite = "model")
+    CustomRule[] getCustomRules();
+}
 
-// interface CustomRule extends WithText {
-//     @Container(opposite = "customRules")
-//     GrammarModel getModel();
-// }
+interface CustomRule extends WithText {
+    @Container(opposite = "customRules")
+    GrammarModel getModel();
+}
 
-// @Immutable
-// interface CodeRange {
-//     CodeLocation getStart();
-//     CodeLocation getStop();
-//     int getLength();
-// }
+@Immutable
+interface CodeRange {
+    CodeLocation getStart();
+    CodeLocation getStop();
+    int getLength();
+}
 
-// @Immutable
-// interface CodeLocation {
-//     int getIndex();
-//     int getLine();
-//     int getCharPosInLine();
-// }
+@Immutable
+interface CodeLocation {
+    int getIndex();
+    int getLine();
+    int getCharPosInLine();
+}
 
-// @InterfaceOnly
-// interface LangElement {
+@InterfaceOnly
+interface LangElement {
 
-// }
+}
 
-// @InterfaceOnly
-// interface CodeElement {
-//     CodeRange getCodeRange();
-// }
+@InterfaceOnly
+interface CodeElement {
+    CodeRange getCodeRange();
+}
 
-// @InterfaceOnly
-// interface WithType extends LangElement {
-//     Type getType();
-// }
+@InterfaceOnly
+interface WithType extends LangElement {
+    Type getType();
+}
 
-// @InterfaceOnly
-// interface WithName extends LangElement {
-//     @GetterOnly
-//     String getName();
+@InterfaceOnly
+interface WithName extends LangElement {
+    @GetterOnly
+    String getName();
 
 
-// // TODO find out why Velocity does not look for methods in super interface
-// //    @DelegateTo(className = "eu.mihosoft.vmf.lang.grammar.NameDelegate")
-// //    String nameWithUpper();
-// }
+// TODO find out why Velocity does not look for methods in super interface
+//    @DelegateTo(className = "eu.mihosoft.vmf.lang.grammar.NameDelegate")
+//    String nameWithUpper();
+}
 
-// @Immutable
-// interface Type extends LangElement, WithName {
+@Immutable
+interface Type extends LangElement, WithName {
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeNameDelegate")
-//     // String nameWithUpper();
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeNameDelegate")
+    // String nameWithUpper();
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeNameDelegate")
-//     // String asModelTypeName();
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeNameDelegate")
+    // String asModelTypeName();
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeNameDelegate")
-//     // String asJavaTypeNameNoCollections();
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeNameDelegate")
+    // String asJavaTypeNameNoCollections();
 
-//     String getPackageName();
+    String getPackageName();
 
-//     String getAntlrRuleName();
+    String getAntlrRuleName();
 
-//     boolean isRuleType();
+    boolean isRuleType();
 
-//     boolean isArrayType();
-// }
+    boolean isArrayType();
+}
 
-// // @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.CheckPropertiesDelegate")
-// interface RuleClass extends WithName, CodeElement {
+// @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.CheckPropertiesDelegate")
+interface RuleClass extends WithName, CodeElement {
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.NameDelegate")
-//     // String nameWithUpper();
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.NameDelegate")
+    // String nameWithUpper();
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.NameDelegate")
-//     // String nameWithLower();
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.NameDelegate")
+    // String nameWithLower();
 
-//     @Contains(opposite = "ruleClasses")
-//     GrammarModel getModel();
+    @Container(opposite = "ruleClasses")
+    GrammarModel getModel();
 
-//     @Contains(opposite = "parent")
-//     Property[] getProperties();
+    @Contains(opposite = "parent")
+    Property[] getProperties();
 
-//     @Contains(opposite = "parent")
-//     Property[] getCustomProperties();
+    @Contains(opposite = "parent")
+    Property[] getCustomProperties();
 
-//     @Container(opposite = "childClasses")
-//     RuleClass getSuperClass();
+    @Container(opposite = "childClasses")
+    RuleClass getSuperClass();
 
-//     String[] getSuperInterfaces();
+    String[] getSuperInterfaces();
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.StringUtilDelegate")
-//     // String superInterfacesString();
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.StringUtilDelegate")
+    // String superInterfacesString();
 
-//     @Contains(opposite = "superClass")
-//     RuleClass[] getChildClasses();
+    @Contains(opposite = "superClass")
+    RuleClass[] getChildClasses();
 
-//     boolean isRoot();
+    boolean isRoot();
 
-//     @Contains(opposite = "parent")
-//     DelegationMethod[] getDelegationMethods();
+    @Contains(opposite = "parent")
+    DelegationMethod[] getDelegationMethods();
 
-//     @Contains(opposite = "parent")
-//     RuleAnnotation[] getCustomRuleAnnotations();
+    @Contains(opposite = "parent")
+    RuleAnnotation[] getCustomRuleAnnotations();
 
-// }
+}
 
-// interface Property extends WithName, WithType, CodeElement {
-//     @Container(opposite = "properties")
-//     RuleClass getParent();
+interface Property extends WithName, WithType, CodeElement {
+    @Container(opposite = "properties")
+    RuleClass getParent();
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.NameDelegate")
-//     // String nameWithUpper();
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.NameDelegate")
+    // String nameWithUpper();
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.NameDelegate")
-//     // String nameWithLower();
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.NameDelegate")
+    // String nameWithLower();
 
-//     Type getType();
+    Type getType();
 
-//     @Contains(opposite = "property")
-//     PropertyAnnotation[] getAnnotations();
-// }
+    @Contains(opposite = "property")
+    PropertyAnnotation[] getAnnotations();
+}
 
-// interface DelegationMethod extends WithText{
-//     @Container(opposite = "delegationMethods")
-//     RuleClass getParent();
-// }
+interface DelegationMethod extends WithText{
+    @Container(opposite = "delegationMethods")
+    RuleClass getParent();
+}
 
-// @InterfaceOnly
-// interface WithText {
-//     String getText();
-// }
+@InterfaceOnly
+interface WithText {
+    String getText();
+}
 
-// interface PropertyAnnotation extends WithText {
+interface PropertyAnnotation extends WithText {
 
-//     @Container(opposite = "annotations")
-//     Property getProperty();
-// }
+    @Container(opposite = "annotations")
+    Property getProperty();
+}
 
-// interface RuleAnnotation extends WithText{
-//     @Container(opposite = "customRuleAnnotations")
-//     RuleClass getParent();
-// }
+interface RuleAnnotation extends WithText{
+    @Container(opposite = "customRuleAnnotations")
+    RuleClass getParent();
+}
 
-// // TypeMapping
+// TypeMapping
 
-// interface TypeMappings {
-//     @Contains(opposite = "parent")
-//     TypeMapping[] getTypeMappings();
+interface TypeMappings {
+    @Contains(opposite = "parent")
+    TypeMapping[] getTypeMappings();
 
-//     @Container(opposite = "typeMappings")
-//     GrammarModel getModel();
+    @Container(opposite = "typeMappings")
+    GrammarModel getModel();
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeMappingsLookup")
-//     // public Optional<Mapping> mappingByRuleName(String containeRuleName, String name);
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeMappingsLookup")
+    // public Optional<Mapping> mappingByRuleName(String containeRuleName, String name);
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeMappingsLookup")
-//     // boolean mappingByRuleNameExists(String containeRuleName, String name);
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeMappingsLookup")
+    // boolean mappingByRuleNameExists(String containeRuleName, String name);
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeMappingsLookup")
-//     // public String targetTypeNameOfMapping(String containeRuleName, String name);
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeMappingsLookup")
+    // public String targetTypeNameOfMapping(String containeRuleName, String name);
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeMappingsLookup")
-//     // public String conversionCodeOfMappingStringToType(String containeRuleName, String name);
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeMappingsLookup")
+    // public String conversionCodeOfMappingStringToType(String containeRuleName, String name);
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeMappingsLookup")
-//     // public String conversionCodeOfMappingTypeToString(String containeRuleName, String name);
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeMappingsLookup")
+    // public String conversionCodeOfMappingTypeToString(String containeRuleName, String name);
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeMappingsLookup")
-//     // public String defaultValueCode(String containerRuleName, String paramRuleName);
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeMappingsLookup")
+    // public String defaultValueCode(String containerRuleName, String paramRuleName);
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeMappingsLookup")
-//     // public List<Mapping> mappingsByRuleName(String ruleName);
-// }
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeMappingsLookup")
+    // public List<Mapping> mappingsByRuleName(String ruleName);
+}
 
-// interface TypeMapping {
+interface TypeMapping {
 
-//     @Container(opposite = "typeMappings")
-//     TypeMappings getParent();
+    @Container(opposite = "typeMappings")
+    TypeMappings getParent();
 
-//     @Contains(opposite = "parent")
-//     Mapping[] getEntries();
+    @Contains(opposite = "parent")
+    Mapping[] getEntries();
 
-// //    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeMappingLookup")
-//     // public Optional<Mapping> mappingByRuleName(String name);
+//    @DelegateTo(className = "eu.mihosoft.vmf.vmftext.grammar.TypeMappingLookup")
+    // public Optional<Mapping> mappingByRuleName(String name);
 
-//     String[] getApplyToNames();
+    String[] getApplyToNames();
 
 
-// }
+}
 
-// interface Mapping {
-//     @Container(opposite="entries")
-//     TypeMapping getParent();
+interface Mapping {
+    @Container(opposite="entries")
+    TypeMapping getParent();
 
-//     String getRuleName();
+    String getRuleName();
 
-//     String getTypeName();
+    String getTypeName();
 
-//     String getTypeToStringCode();
+    String getTypeToStringCode();
 
-//     String getStringToTypeCode();
+    String getStringToTypeCode();
 
-//     String getDefaultValueCode();
-// }
+    String getDefaultValueCode();
+}
 
 
 
