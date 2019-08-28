8f72cf54e4cd745720d8bc923709efa3ee21d413
Michael Hoffer <info@michaelhoffer.de>
2019-08-28 20:16:20 +0200

    added reverse lazy test

--- test-suite/src/test/java/eu/mihosoft/vmftest/lazyinit/LazyInitTest.java
+++ test-suite/src/test/java/eu/mihosoft/vmftest/lazyinit/LazyInitTest.java
@@ -6,10 +6,24 @@ import static org.hamcrest.Matchers.*;
 import org.junit.Test;
 
 public class LazyInitTest {
-    @Test public void testLazyInitOfLists() {
+    @Test public void testLazyInitOfLists1() {
         Obj o1 = Obj.newInstance();
         Obj o2 = Obj.newInstance();
-        System.out.println("size: "+ o1.getEntries().size());
+        // call getEntries() to ensure that lazy init is done for o1
+        // we don't do that for o2 to ensure that we compare with
+        // o2.entries == null
+        System.out.println("size: " + o1.getEntries().size());
+
+        boolean equal = o1.equals(o2);
+        assertThat(equal,equalTo(true));
+    }
+    @Test public void testLazyInitOfLists2() {
+        Obj o1 = Obj.newInstance();
+        Obj o2 = Obj.newInstance();
+        // call getEntries() to ensure that lazy init is done for o2
+        // we don't do that for o1 to ensure that we compare with
+        // o1.entries == null
+        System.out.println("size: " + o2.getEntries().size());
 
         boolean equal = o1.equals(o2);
         assertThat(equal,equalTo(true));
