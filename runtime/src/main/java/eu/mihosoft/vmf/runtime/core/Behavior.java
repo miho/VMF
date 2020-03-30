2ff1648e710b411e47165ffd86f9c0e816bb4f26
Michael Hoffer <info@michaelhoffer.de>
2020-03-30 16:34:04 +0200

    dynamic behaviors come as a preview feature:
    - vmf().behavior().set() and get() have been added to allow custom behaviors
      which can be changed at runtime
    - each interface has an additional nested behavior interface
    - static behavior is still required and hasn't been changed yet

--- runtime/src/main/java/eu/mihosoft/vmf/runtime/core/Behavior.java
+++ runtime/src/main/java/eu/mihosoft/vmf/runtime/core/Behavior.java
@@ -1,6 +1,6 @@
 package eu.mihosoft.vmf.runtime.core;
 
 public interface Behavior<T extends VObject> {
-    DelegatedBehavior<T> getBehavior();
-    void setBehavior(DelegatedBehavior<T> delegate);
+    DelegatedBehavior<T> get();
+    void set(DelegatedBehavior<T> delegate);
 }
