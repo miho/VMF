165550824766da9c2d55250ec9e3141633adb865
miho <info@michaelhoffer.de>
2021-11-08 14:10:58 +0100

    fix for https://github.com/miho/VMF/issues/47 #47

--- test-suite/src/test/java/eu/mihosoft/vmftest/observableprop/ObservablePropTest.java
+++ test-suite/src/test/java/eu/mihosoft/vmftest/observableprop/ObservablePropTest.java
@@ -115,11 +115,94 @@ public class ObservablePropTest {
         // finally, we removed 2 elements
         Assert.assertEquals(2, actualValues1.get(2).size());
         Assert.assertEquals(2, actualValues2.get(2).size());
+    }
+
+    @Test
+    public void observeSimplePropertyOfReadOnlyTest() {
+        ObserveMyProperties observeMyProperties = ObserveMyProperties.newInstance();
+        ReadOnlyObserveMyProperties observeMyPropertiesRO = observeMyProperties.asReadOnly();
+
+        Property nameProperty = observeMyPropertiesRO.vmf().reflect().propertyByName("name").orElse(null);
+
+        Assert.assertNotNull(nameProperty);
+
+        List<String> expectedValues = Arrays.asList("ABC", "123", "", null);
+
+        List<String> actualValues1 = new ArrayList<>();
+        List<String> actualValues2 = new ArrayList<>();
+
+        observeMyPropertiesRO.vmf().changes().addListener(
+            (change) -> actualValues1.add((String) change.propertyChange().get().newValue())
+        );
+
+        nameProperty.addChangeListener(change ->
+            actualValues2.add((String) change.propertyChange().get().newValue())
+        );
+
+        for (String expected : expectedValues) {
+            observeMyProperties.setName(expected);
+        }
+
+        Assert.assertEquals(expectedValues, actualValues1);
+        Assert.assertEquals(expectedValues, actualValues2);
 
     }
 
     @Test
-    public void throwExceptionIfRuntimeMEthodsAreUsedForStaticReflection() {
+    public void observeListPropertyReadOnlyTest() {
+        ObserveMyProperties observeMyProperties = ObserveMyProperties.newInstance();
+        ReadOnlyObserveMyProperties observeMyPropertiesRO = observeMyProperties.asReadOnly();
+
+        Property values = observeMyPropertiesRO.vmf().reflect().propertyByName("values").orElse(null);
+
+        Assert.assertNotNull(values);
+
+        List<List<Integer>> actualValues1 = new ArrayList<>();
+        List<List<Integer>> actualValues2 = new ArrayList<>();
+
+        observeMyPropertiesRO.vmf().changes().addListener(
+            (change) -> {
+                if (change.listChange().get().wasAdded()) {
+                    actualValues1.add((List<Integer>) (Object) change.listChange().get().added().elements());
+                } else if (change.listChange().get().wasRemoved()) {
+                    actualValues1.add((List<Integer>) (Object) change.listChange().get().removed().elements());
+                }
+            }
+        );
+
+        values.addChangeListener(
+            (change) -> {
+                if (change.listChange().get().wasAdded()) {
+                    actualValues2.add((List<Integer>) (Object) change.listChange().get().added().elements());
+                } else if (change.listChange().get().wasRemoved()) {
+                    actualValues2.add((List<Integer>) (Object) change.listChange().get().removed().elements());
+                }
+            }
+        );
+
+        observeMyProperties.getValues().addAll(Arrays.asList(1, 2, 3));
+        observeMyProperties.getValues().remove(2);
+        observeMyProperties.getValues().removeAll(0, 1);
+
+        // we made 3 modifications -> 3 changes
+        Assert.assertEquals(3, actualValues1.size());
+        Assert.assertEquals(3, actualValues2.size());
+
+        // first, we added 3 elements
+        Assert.assertEquals(3, actualValues1.get(0).size());
+        Assert.assertEquals(3, actualValues2.get(0).size());
+
+        // next, we removed 1 elements
+        Assert.assertEquals(1, actualValues1.get(1).size());
+        Assert.assertEquals(1, actualValues2.get(1).size());
+
+        // finally, we removed 2 elements
+        Assert.assertEquals(2, actualValues1.get(2).size());
+        Assert.assertEquals(2, actualValues2.get(2).size());
+    }
+
+    @Test
+    public void throwExceptionIfRuntimeMethodsAreUsedForStaticReflection() {
 
         Property nameProperty = ObserveMyProperties.type().reflect().propertyByName("name").orElse(null);
 
