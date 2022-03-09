388269ff2b2f807a481a401f9a70bd31c0b6a208
Michael Hoffer <info@michaelhoffer.de>
2022-03-09 13:25:12 +0100

    fixed https://github.com/miho/VMF/issues/50

--- test-suite/src/test/vmf/eu/mihosoft/vmftest/equals/vmfmodel/EqualsTestModel.java
+++ test-suite/src/test/vmf/eu/mihosoft/vmftest/equals/vmfmodel/EqualsTestModel.java
@@ -50,3 +50,39 @@ interface EqualsTestModelInstanceEq extends WithName {
 
     AReference getReference();
 }
+
+
+@VMFEquals(CONTAINMENT_AND_EXTERNAL)
+interface EqualsTestContainmentEqListChild extends WithName {
+
+    @Container(opposite = "children")
+    EqualsTestContainmentEqList getParent();
+
+}
+
+
+@VMFEquals(CONTAINMENT_AND_EXTERNAL)
+interface EqualsTestContainmentEqList extends WithName {
+
+    @Contains(opposite = "parent")
+    EqualsTestContainmentEqListChild[] getChildren();
+
+}
+
+@VMFEquals(INSTANCE)
+interface EqualsTestInstanceEqListChild extends WithName {
+
+    @Container(opposite = "children")
+    EqualsTestInstanceEqList getParent();
+
+}
+
+
+@VMFEquals(INSTANCE)
+interface EqualsTestInstanceEqList extends WithName {
+
+    @Contains(opposite = "parent")
+    EqualsTestInstanceEqListChild[] getChildren();
+
+}
+
