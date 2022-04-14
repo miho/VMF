0a419fd4e6dc7960741e955a89cf2470fd0c3986
Michael Hoffer <info@michaelhoffer.de>
2022-04-14 12:43:29 +0200

    builders compile with new API

--- runtime/src/main/java/eu/mihosoft/vmf/runtime/core/Builder.java
+++ runtime/src/main/java/eu/mihosoft/vmf/runtime/core/Builder.java
@@ -4,5 +4,7 @@ package eu.mihosoft.vmf.runtime.core;
  *
  */
 public interface Builder {
-    VObject build();
+    default VObject build() {
+        throw new UnsupportedOperationException("Cannot build interface-only type");
+    }
 }
