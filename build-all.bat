9168db27844f78a56f137ad5fc8b9c2754c52afc
Michael Hoffer <info@michaelhoffer.de>
2025-06-08 22:27:43 +0200

    build scripts without testing added for codex improvements

--- build-all.bat
+++ build-all.bat
@@ -8,7 +8,9 @@ cmd /c "gradlew.bat" clean publishtoMavenLocal --no-daemon || exit /b %ERRORLEVE
 echo ------------------------------------------
 echo BUILDING AND TESTING JACKSON MODULE
 echo ------------------------------------------
-cd ..\jackson
+cd jackson
 @REM cmd /c "gradlew.bat" clean test --stacktrace --no-daemon --warning-mode all || exit /b %ERRORLEVEL%;
 cmd /c "gradlew.bat" clean publishtoMavenLocal --no-daemon || exit /b %ERRORLEVEL%;
+cd ..\
+
 
